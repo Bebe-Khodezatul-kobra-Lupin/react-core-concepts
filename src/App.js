0564e59e8c750f4812f7a4e92911199a34218746
Lupin <kobra15-11711@diu.edu.bd>
2023-03-24 15:40:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <CounterNum></CounterNum>
+    </div>
+  );
+}
+
+export default App;
+
+
+function CounterNum() {
+
+  const [count, setCount] = useState(55);
+  //console.log(abc);
+  const incressCount = () => {
+    const newcount = count + 1;
+    setCount(newcount);
+  }
+  return (
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={incressCount}  >Incress</button>
+
+    </div>
+  )
+}
+
+
+
+
+
+//component and prop code 
+
+{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <p></p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,9 +51,4 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      </header> */}
